37cd9ca53aae995a7b3bba4e0656029733b138a7
Александр Кудрявцев <aleksandrkudravcev@MacBook-Pro-Aleksandr.local>
2021-10-03 19:34:27 +0300

    4.6 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(),
-      body: const MyApp(),
-    ),
-  ));
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -17,25 +12,50 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final List<String> nav = ['Список #1', 'Список #2', 'sdfsfd'];
   List<String> fakeData = List.generate(100, (index) => index.toString());
+  List<String> fileNames = [
+    'assets/images/s1.png',
+    'assets/images/s2.png',
+    'assets/images/s3.png',
+    'assets/images/s4.png',
+    'assets/images/s5.png',
+    'assets/images/s6.png',
+    'assets/images/s7.png',
+    'assets/images/s8.png',
+    'assets/images/s9.png',
+    'assets/images/s10.png',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: nav.length,
+      length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('list_view_keys_scroll'),
-          bottom: TabBar(
-            tabs: nav.map((String item) => Tab(text: item)).toList(),
+          title: const Text('Appbar title'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.access_alarm_sharp)),
+              Tab(icon: Icon(Icons.accessible_forward_rounded)),
+            ],
           ),
         ),
-        body: TabBarView(children: [
-          Text('111'),
-          Text('222'),
-          Text('333')
-        ]),
+        body: TabBarView(
+          children: [
+            ListView(
+              key: const PageStorageKey('1'),
+              children: [
+                for (var item in fileNames) Image(image: AssetImage(item)),
+              ],
+            ),
+            ListView(
+              key: const PageStorageKey('2'),
+              children: [
+                for (var item in fileNames) Image(image: AssetImage(item)),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
